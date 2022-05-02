5d0a8699c03e922d509c60d2800283b7b494849d
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-02 12:56:30 +0300

    add database

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 
-from app import create_app
+from app import create_app, db
 from flask_script import Manager,Server
+
+from app.models import User
 
 #creating app instance
 app = create_app('development')
@@ -18,6 +20,8 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User= User)
 if __name__ == '__main__':
     manager.run()
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY=767b94862f4adf99f2cdbe520480ce56
-export SECRET_KEY=lynne
+export MOVIE_API_KEY='767b94862f4adf99f2cdbe520480ce56'
+export SECRET_KEY='lynne'
 
 python3.9 manage.py server
