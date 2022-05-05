e2895bdfe81d80634bf8108843301be3d970d84c
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-05 11:02:16 +0300

    update initfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -10,38 +10,45 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-photos = UploadSet('photos',IMAGES)
 
 
-#bootstrap = Bootstrap()
+# bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 mail = Mail()
 
 def create_app(config_name):
 
     app = Flask(__name__)
 
-    mail.init_app(app)
-
     # Creating the app configurations
+
     app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
-    #bootstrap.init_app(app)
+    # bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    
+    mail.init_app(app)
+
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #setting config
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+    # setting config
     from .request import configure_request
     configure_request(app)
-    
-     # configure UploadSet
+
+
+
+    # configure UploadSet
     configure_uploads(app,photos)
-    # Will add the views and forms
+
+
 
     return app
